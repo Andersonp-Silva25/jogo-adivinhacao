5e8c66056dc6c7b3e8af857cd1a1a0ad6865b7d5
Anderson Silva <andersonp.silva25@gmail.com>
2026-06-11 22:15:57 -0300

    Gerando um numero secreto aleatorio

--- projeto.py
+++ projeto.py
@@ -3,3 +3,8 @@
 # No jogo, o usuario precisa adivinhar um numero secreto.
 # Ele pode tentar varias vezes ate acertar.
 
+import random
+
+random_number = random.randint(0,10)
+
+print(random_number)
