414492bf6ba125c184668947a6ef11648ac4ad63
Anderson Silva <andersonp.silva25@gmail.com>
2026-06-11 22:29:59 -0300

    Adicionando estrutura de repeticao while e condicionais if

--- projeto.py
+++ projeto.py
@@ -5,6 +5,15 @@
 
 import random
 
-random_number = random.randint(0,10)
+secret_number = random.randint(1,10)
+correct_number = 0
 
-print(random_number)
+while secret_number != correct_number:
+    correct_number = int(input("Tente adivinhar o numero secreto de 1 a 10: "))
+
+    if correct_number < secret_number:
+        print("Voce errou, o numero secreto é maior do que o numero que voce tentou")
+    elif correct_number > secret_number:
+        print("Voce errou, o numero secreto é menor do que o numero que voce tentou")
+    else:
+        print("Parabéns, voce acertou o numero secreto")
